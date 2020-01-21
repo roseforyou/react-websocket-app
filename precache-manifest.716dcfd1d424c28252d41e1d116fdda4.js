903ba53af0768c53f21f739b96d4c87be7491ad1
roseforyou <1073140+roseforyou@users.noreply.github.com>
2020-01-21 08:51:16 +0000

    Deploying to gh-pages from master f1477a275d03b72fa972c4848510143363014e03

---
+++ precache-manifest.716dcfd1d424c28252d41e1d116fdda4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e7b879382da61abf0c0148056ef0d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30ae6c88fdb59222f09d",
+    "url": "./static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "a923312051d0911c3677",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30ae6c88fdb59222f09d",
+    "url": "./static/js/2.302cb94f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.302cb94f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a923312051d0911c3677",
+    "url": "./static/js/main.27a65320.chunk.js"
+  },
+  {
+    "revision": "779ea8f99c718cfffd8e",
+    "url": "./static/js/runtime-main.98cdd643.js"
+  }
+]);
