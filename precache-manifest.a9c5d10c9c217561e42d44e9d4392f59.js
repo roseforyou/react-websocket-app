76a39877e2e703c483108a3a9775abb3d250644b
roseforyou <nhst.js@gmail.com>
2021-12-29 08:13:54 +0000

    Deploying to gh-pages from master 63c6f17499d179873c22a236fa77eac7d321e4a0

---
+++ precache-manifest.a9c5d10c9c217561e42d44e9d4392f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4aa0293c7174547f20db10f480e50c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30ae6c88fdb59222f09d",
+    "url": "./static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "927e224f82ddfd527207",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30ae6c88fdb59222f09d",
+    "url": "./static/js/2.302cb94f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.302cb94f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "927e224f82ddfd527207",
+    "url": "./static/js/main.e798e68e.chunk.js"
+  },
+  {
+    "revision": "779ea8f99c718cfffd8e",
+    "url": "./static/js/runtime-main.98cdd643.js"
+  }
+]);
